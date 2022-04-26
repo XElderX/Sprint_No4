8a29df3361c55857ca1d149386cf0eb78571dbce
XElderX <dalius.badley@gmail.com>
2022-04-26 23:28:17 +0300

    UPDATE added clear all button,implemented localStorage for all methods,auto load all data after reload, added count item. and more.

--- assets/main.js
+++ assets/main.js
@@ -11,6 +11,7 @@ settime.innerHTML = time;
 
 //Variables and styling them
 let items = []; // local storage variable to store items
+let itemsCount = document.querySelector(".list-number");
 const inputLine = document.querySelector("input");
 const itemList = document.querySelector(".added-Items");
 itemList.style.color = "black";
@@ -18,30 +19,91 @@ itemList.style.fontSize = "24px"
 itemList.style.fontWeight = "bold";
 itemList.style.margin = "15px"
 const button = document.querySelector(".addItem");
+//creating clear all button
+const clearAllBtn = document.createElement('button');
+button.parentNode.insertBefore(clearAllBtn,button.nextSibling)
+clearAllBtn.innerText = "Clear all List"
+clearAllBtn.classList.add('btn', 'btn-danger', 'm-3', 'btn-lg')
+clearAllBtn.style.display="none";
 
 //Events
-
+//add and store to local store
 button.addEventListener("click", (e) =>{
     e.preventDefault();
-    let newItem = document.createElement("li");
-    newItem.textContent = inputLine.value;
-    items.push(inputLine.value);//storage
-    localStorage.setItem('itemsToShop', JSON.stringify(items));//storage
-    const linkDelete = document.createElement("a");
-    linkDelete.className = 'deleteIcon';
-    linkDelete.innerHTML = '<i class="fa fa-trash-o"></i>'; 
-    linkDelete.style.color="red";
-    linkDelete.style.float ="right";
-    newItem.appendChild(linkDelete);
-    itemList.appendChild(newItem);
-    inputLine.value = ""
+    if(inputLine.value!=""){
+        if(localStorage.getItem('itemsToShop') === null) {
+            items = [];
+        }
+        else {
+            items = JSON.parse(localStorage.getItem('itemsToShop'));
+        }
+        let newItem = document.createElement("li");
+        newItem.textContent = inputLine.value;
+        items.push(inputLine.value);//storage
+        localStorage.setItem('itemsToShop', JSON.stringify(items));//storage
+        const linkDelete = document.createElement("a");
+        linkDelete.className = 'deleteIcon';
+        linkDelete.innerHTML = '<i class="fa fa-trash-o"></i>'; 
+        linkDelete.style.color="red";
+        linkDelete.style.float ="right";
+        newItem.appendChild(linkDelete);
+        itemList.appendChild(newItem);
+        inputLine.value = ""
+        clearAllBtn.style.display="inline";
+    }
+    itemsCount.innerText= checkItemsCount();
+        clearbtnDisplay();
 })
+
+// remove item from the list
+itemList.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(e.target.parentElement.classList.contains('deleteIcon')) {
+        //console.log(e.target.parentElement.parentElement)
+        e.target.parentElement.parentElement.remove()
+        removeItemLocal(e.target.parentElement.parentElement)
+        function removeItemLocal(itemDelete) {
+            console.log(itemDelete.textContent)
+            let localStorageToRemove =JSON.parse(localStorage.getItem('itemsToShop'));
+            console.log(localStorageToRemove)
+            localStorageToRemove.forEach(function(
+                item){
+                if(itemDelete.textContent === item){
+                    localStorageToRemove.splice(localStorageToRemove.indexOf(itemDelete.textContent), 1)
+                    localStorage.setItem('itemsToShop', JSON.stringify(localStorageToRemove));
+                    itemsCount.innerText= checkItemsCount();
+                }  
+            })
+        };
+
+    }
+    itemsCount.innerText= checkItemsCount();
+    clearbtnDisplay();
+ 
+});
+
+//remove all items 
+
+clearAllBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+    
+    
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild);
+    }
+    clearLocalStorage();
+    itemsCount.innerText= checkItemsCount();
+    clearbtnDisplay();
+});
+
+// functions
 //Storage load
 
 let displayStoragedItems = ()=>{
     let loadFromStorage = JSON.parse(localStorage.getItem("itemsToShop"));
     if(loadFromStorage != null){
-
+        clearAllBtn.style.display="inline"
+        
         for(i=0; i<loadFromStorage.length; i++) {
             let newItem = document.createElement("li");
             newItem.appendChild(document.createTextNode(loadFromStorage[i]))
@@ -53,8 +115,28 @@ let displayStoragedItems = ()=>{
             linkDelete.style.float ="right";
             newItem.appendChild(linkDelete);
         };
-        items=loadFromStorage;
+        clearbtnDisplay();
+        itemsCount.innerText=checkItemsCount();
     }
-
 }
+//count items in list
+function checkItemsCount () {
+     if (localStorage.getItem('itemsToShop') === null) {
+         return 0;
+     }
+     else {
+        return JSON.parse(localStorage.getItem("itemsToShop")).length;
+     }
+}
+//storage clear
+function clearLocalStorage(){
+    localStorage.clear();
+}
+//button clear list display
+function clearbtnDisplay (){
+let check = checkItemsCount ()
+check >0 ? clearAllBtn.style.display = "inline" 
+: clearAllBtn.style.display = "none" 
+}
+checkItemsCount();
 displayStoragedItems();
