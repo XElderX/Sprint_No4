fce6e8f2c5f21603257ee23cdbc77e8ab637b1a4
XElderX <dalius.badley@gmail.com>
2022-04-26 23:18:08 +0300

    added add item function. Now you can add item to the list. NOTE localStorage have not implemented yet

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,4 @@
+// Clock and date
 const date = new Date().toDateString() ;
 const setdate = document.querySelector(".setdate");
 setdate.innerHTML = date;
@@ -8,3 +9,22 @@ const settime = document.querySelector(".settime");
 settime.innerHTML = time;
 },500);
 
+//Variables and styling them
+
+const inputLine = document.querySelector("input");
+const itemList = document.querySelector(".added-Items");
+itemList.style.color = "black";
+itemList.style.fontSize = "24px"
+itemList.style.fontWeight = "bold";
+itemList.style.margin = "15px"
+const button = document.querySelector(".addItem");
+
+//Events
+
+button.addEventListener("click", (e) =>{
+    e.preventDefault();
+    let newItem = document.createElement("li");
+    newItem.textContent = inputLine.value;
+    itemList.appendChild(newItem);
+    inputLine.value = ""
+})
